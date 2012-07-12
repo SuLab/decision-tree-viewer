b1deda80c4f574ffece40d63d96c8e8d630b78eb
Max Nanis <max@maxnanis.com>
2012-07-12 12:17:29 -0700

    new data response, starting to think about changing draw objects based on node types

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-var json = {"name":"feathers","children":[{"name":"= false","children":[{"name":"milk","children":[{"name":"= false","children":[{"name":"backbone","children":[{"name":"= false","children":[{"name":"airborne","children":[{"name":"= false","children":[{"name":"predator","children":[{"name":"= false","children":[{"name":"legs","children":[{"name":"<= 2","children":[{"name":"invertebrate (2.0)"}]},{"name":"> 2","children":[{"name":"insect (2.0)"}]}]}]},{"name":"= true","children":[{"name":"invertebrate (8.0)"}]}]}]},{"name":"= true","children":[{"name":"insect (6.0)"}]}]}]},{"name":"= true","children":[{"name":"fins","children":[{"name":"= false","children":[{"name":"tail","children":[{"name":"= false","children":[{"name":"amphibian (3.0)"}]},{"name":"= true","children":[{"name":"reptile (6.0/1.0)"}]}]}]},{"name":"= true","children":[{"name":"fish (13.0)"}]}]}]}]}]},{"name":"= true","children":[{"name":"mammal (41.0)"}]}]}]},{"name":"= true","children":[{"name":"bird (20.0)"}]}]}
+var json = {"evaluation" : {"modelrep":"J48 pruned tree\n------------------\n\nMASP1 <= 0.074\n|   E2F1 <= -0.299: non-relapse (6.0)\n|   E2F1 > -0.299: relapse (44.0/14.0)\nMASP1 > 0.074: non-relapse (28.0/4.0)\n\nNumber of Leaves  : \t3\n\nSize of the tree : \t5\n","accuracy":64}, "max_depth":"5","num_leaves":"3","tree_size":"5","tree":{"name":"MASP1","kind":"split_node","children":[{"name":"<= 0.074","kind":"split_value","children":[{"name":"E2F1","kind":"split_node","children":[{"name":"<= -0.299","kind":"split_value","children":[{"name":"non-relapse","kind":"leaf_node","bin_size":6.0,"errors":0.0}]},{"name":"> -0.299","kind":"split_value","children":[{"name":"relapse","kind":"leaf_node","bin_size":44.0,"errors":14.0}]}]}]},{"name":"> 0.074","kind":"split_value","children":[{"name":"non-relapse","kind":"leaf_node","bin_size":28.0,"errors":4.0}]}]}}
+
+function kind(kind_text) {
+  switch(kind_text)
+  {
+    case "split_node":
+      return 0;
+    case "split_value":
+      return 1;
+    case "split_node":
+      return 2;
+    default:
+      return 3;
+  }
+  return 3;
+}
 
 //@Ben -- how will this get triggered?
 $(document).ready(function() {
   var green = "#1FA13A",
   orange = "#D44413",
   width = 900,
-  height = 800;
+  height = 800,
+  depth = json.max_depth-1;
 
   var cluster = d3.layout.tree()
     .size([width-40, height-40]);
@@ -19,13 +35,11 @@ $(document).ready(function() {
     .attr("transform", "translate(20,20)");
 
   //Deeeebugging
-  var nodes = cluster.nodes(json);
+  var nodes = cluster.nodes(json.tree);
   console.log(nodes);
   var links = cluster.links(nodes);
   console.log(links);
-  //updated: will just have push this into json.metadata.tree_depth
-  var network_depth = _.max(_.pluck(nodes, "depth"));
-  var color = d3.scale.linear().domain([0, network_depth]).range([orange, green]);
+  var color = d3.scale.linear().domain([0, depth]).range([orange, green]);
 
   //Draw the links first so they're behind the nodes
   var link = vis.selectAll("path.link")
@@ -35,7 +49,7 @@ $(document).ready(function() {
   .attr("class", "link")
   .attr("d", diagonal)
   .style("stroke", function(d) { return color(d.source.depth) } )
-  .style("stroke-width", function(d) { return 1.3*(network_depth - d.source.depth+1) +"px" } );
+  .style("stroke-width", function(d) { return 1.3*(depth - d.source.depth+1) +"px" } );
 
   var node = vis.selectAll("g.node")
     .data(nodes)
@@ -47,8 +61,8 @@ $(document).ready(function() {
     //.style("fill", function(d) { return color( d.depth ) } )
     .style("fill", "steelblue" )
     .transition()
-    .delay(10*network_depth)
-    .duration(100*network_depth)
+    .delay(10*depth)
+    .duration(100*depth)
     .attr("ry", function(d) { return 12 } )
     .attr("rx", function(d) { return (($.trim(d.name).length * 2.6) < 14 ? 14 : ($.trim(d.name).length * 2.6) ) });
 
@@ -59,6 +73,6 @@ $(document).ready(function() {
     .attr("dy", 3)
     .style("font-family", "Helvetica")
     .style("font-weight", "bold")
-    .style("fill", "#fff")
-    .text(function(d) { return d.name; });
+    .style("fill", "#000")
+    .text(function(d) { return (kind(d.kind) + " // " + d.name) });
 })
